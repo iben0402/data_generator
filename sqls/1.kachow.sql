7a99912dfc6d76d5f4bbf9c39350e6693dc33842
iben0402 <81482747+iben0402@users.noreply.github.com>
2023-12-04 18:43:58 +0100

    fixed generator

--- sqls/1.kachow.sql
+++ sqls/1.kachow.sql
@@ -8,11 +8,3 @@ BULK INSERT dbo.Problemy FROM '../generated/problems.bulk' WITH (FIELDTERMINATOR
 BULK INSERT dbo.Operacje FROM '../generated/operations.bulk' WITH (FIELDTERMINATOR='|')
 
 GO
-
-BULK INSERT dbo.Uzytkownicy FROM '../generated_2/users.bulk' WITH (FIELDTERMINATOR='|')
-BULK INSERT dbo.Pojazdy FROM '../generated_2/vehicles.bulk' WITH (FIELDTERMINATOR='|')
-BULK INSERT dbo.Przejazdy FROM '../generated_2/drives.bulk' WITH (FIELDTERMINATOR='|')
-BULK INSERT dbo.Problemy FROM '../generated_2/problems.bulk' WITH (FIELDTERMINATOR='|')
-BULK INSERT dbo.Operacje FROM '../generated_2/operations.bulk' WITH (FIELDTERMINATOR='|')
-
-GO
---
+++ sqls/2.kachow.sql
@@ -0,0 +1,10 @@
+USE Kachow
+GO
+
+BULK INSERT dbo.Uzytkownicy FROM '../generated_2/users.bulk' WITH (FIELDTERMINATOR='|')
+BULK INSERT dbo.Pojazdy FROM '../generated_2/vehicles.bulk' WITH (FIELDTERMINATOR='|')
+BULK INSERT dbo.Przejazdy FROM '../generated_2/drives.bulk' WITH (FIELDTERMINATOR='|')
+BULK INSERT dbo.Problemy FROM '../generated_2/problems.bulk' WITH (FIELDTERMINATOR='|')
+BULK INSERT dbo.Operacje FROM '../generated_2/operations.bulk' WITH (FIELDTERMINATOR='|')
+
+GO
